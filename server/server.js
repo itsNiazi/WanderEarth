14391c89b82de065706fcf3d2e32bc53196cac9e
itsNiazi <niazitareqmejl@gmail.com>
2023-11-08 20:20:25 +0100

    First completion

--- server/server.js
+++ server/server.js
@@ -3,11 +3,37 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
+const openApiKey = process.env.OPEN_API_KEY;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
+});
+
+app.post("/completion", async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${openApiKey}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo-instruct",
+      prompt: "What is the capital of Sweden?",
+      max_tokens: 10,
+    }),
+  };
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/completions",
+      options
+    );
+    const jsonResponse = await response.json();
+    res.send(jsonResponse);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.get("/*", (req, res) => {
