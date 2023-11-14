8ad708c203e2103a08e36d9dc838d6bdaab7b8dc
itsNiazi <niazitareqmejl@gmail.com>
2023-11-14 20:53:08 +0100

    prompt, role and display functionality implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,51 @@
+import React, { useState } from "react";
 function App() {
+  const [display, setDisplay] = useState("");
+  const getPrompt = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const prompt = formData.get("prompt");
+
+    const promptResponse = await fetch("http://localhost:3000/completion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt }),
+    });
+
+    const jsonPromptResponse = await promptResponse.json();
+    console.log(jsonPromptResponse);
+    const contentObject = JSON.parse(
+      jsonPromptResponse.choices[0].message.content
+    );
+    setDisplay(contentObject.suggestion);
+  };
+
   return (
-    <h1 className="text-3xl font-bold underline bg-red-500">Hello, world!</h1>
+    <>
+      <div className="min-h-screen bg-cyan-950 text-gray-50">
+        <h1 className=" no-underline text-3xl font-bold  text-amber-300 py-2">
+          WanderEarth
+        </h1>
+        <form
+          className="py-4 text-center"
+          method="POST"
+          action="http://localhost:3000/completion"
+          onSubmit={getPrompt}
+        >
+          <input
+            className="bg-teal-700 rounded p-1 mx-4"
+            id="prompt"
+            name="prompt"
+          ></input>
+          <button type="submit">Send</button>
+        </form>
+        <div className="max-w-xl mx-auto text-center">
+          <p className="text-3xl">{display}</p>
+        </div>
+      </div>
+    </>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
@@ -6,12 +7,22 @@ const port = process.env.PORT;
 const openApiKey = process.env.OPEN_API_KEY;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
 app.post("/completion", async (req, res) => {
+  const prompt = req.body.prompt;
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a travel suggestion assistant You are to only provide travel suggestions! Provide responses in JSON format with 'suggestion' and 'location'.",
+    },
+    { role: "user", content: prompt },
+  ];
   const options = {
     method: "POST",
     headers: {
@@ -19,14 +30,13 @@ app.post("/completion", async (req, res) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      model: "gpt-3.5-turbo-instruct",
-      prompt: "What is the capital of Sweden?",
-      max_tokens: 10,
+      model: "gpt-3.5-turbo",
+      messages: messages,
     }),
   };
   try {
     const response = await fetch(
-      "https://api.openai.com/v1/completions",
+      "https://api.openai.com/v1/chat/completions",
       options
     );
     const jsonResponse = await response.json();
